8669e96c8212203cf6393c03e5cbb5aea90391cd
aaclay1@gmail.com <aaclay1@gmail.com>
2024-08-14 16:46:17 +0000

    Edited settings and EntryImport

--- client_code/EntryImport/__init__.py
+++ client_code/EntryImport/__init__.py
@@ -21,7 +21,7 @@ class EntryImport(EntryImportTemplate):
     
     # Split the content into lines
     rows = csv_content.splitlines()
-    
+    print(rows[1])
     # Get the headers from the first line
     headers = rows[0].split(',')
     
@@ -33,9 +33,10 @@ class EntryImport(EntryImportTemplate):
       # Create a dictionary to map column names to values
       row_data = {header: value for header, value in zip(headers, values)}
       # Insert the row into the DataTable
+      print(row_data)
       content =re.sub(r'{return}', '\r', row_data['content'].replace("{comma}", ","))
       content = content[:len(content)-1]
-      app_tables.entries.add_row(startYear=int(row_data['startYear']),endYear=int(row_data['endYear']),firstName=row_data['firstName'],lastName=row_data['lastName'],middleName=row_data['middleName'],occupation=row_data['occupation'],geoLocation=row_data['geoLocation'],content=content,created=datetime.now(),updated=datetime.now())
+      app_tables.entries.add_row(startYear=row_data['startYear'],endYear=row_data['endYear'],firstName=row_data['firstName'],lastName=row_data['lastName'],middleName=row_data['middleName'],occupation=row_data['occupation'],geoLocation=row_data['geoLocation'],content=content,created=datetime.now(),updated=datetime.now())
       
     alert("CSV file uploaded and data saved!")
     open_form('NotationsForm')
